d229af57f25da6def9a2985ca32814d0ac9b24d6
unknown <xogk1128@naver.com>
2022-07-22 16:50:13 +0900

    보안

--- create.php
+++ create.php
@@ -3,16 +3,6 @@ require('lib/print.php');
 require('view/top.php');
 ?>
 
-<!doctype html>
-<html>
-    <head>
-        <title><?php print_title()?></title>
-    </head>
-    <body>
-        <h1><a href="test.php">Home</a></h1>
-        <ol>
-            <?php print_list();?>
-        </ol>
         <a href="create.php">create</a>
         <?php if(isset($_GET['id'])){ ?>
             <a href="update.php?id=<?=$_GET['id']?>">update</a>
--- lib/print.php
+++ lib/print.php
@@ -1,7 +1,7 @@
 <?php
     function print_title(){
         if(isset($_GET['id'])){
-            echo $_GET['id'];
+            echo htmlspecialchars($_GET['id']);
         } else{
             echo "Welcome";
             }
@@ -9,11 +9,11 @@
 
     function print_list(){
         $list = scandir("./data");
-        //$contents = file_get_contents("data/".$_GET['id']);
         $i = 0;
         while($i<count($list)){
+            $title = htmlspecialchars($list[$i]);
             if($list[$i] != '.' && $list[$i] != '..'){
-            echo "<li><a href=\"?id=$list[$i]\">$list[$i]</a></li>";
+            echo "<li><a href=\"?id=$title\">$title</a></li>";
             }
             $i += 1;
         }
@@ -21,7 +21,8 @@
 
     function print_description(){
         if(isset($_GET['id'])){
-            echo file_get_contents("data/".$_GET['id']);
+            $basename = basename($_GET['id']);
+            echo htmlspecialchars(file_get_contents("data/".$basename));
             } else {
                 echo "WEB is...";
             } 
