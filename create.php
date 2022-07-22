bf6cbf7a9306b66e4bfdeea3cd6b033176869dfe
unknown <xogk1128@naver.com>
2022-07-22 15:36:10 +0900

    CRUD만들기

--- create.php
+++ create.php
@@ -38,8 +38,12 @@
         <ol>
             <?php print_list();?>
         </ol>
-        <p><a href="create.php">create</a></p>
-        <form action="create_proccess.php" method="post">
+        <a href="create.php">create</a>
+        <?php if(isset($_GET['id'])){ ?>
+            <a href="update.php?id=<?=$_GET['id']?>">update</a>
+        <?php } ?>
+
+        <form action="create_process.php" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p><textarea name="description" placeholder="description"></textarea></p>
             <p><input type="submit" value="submit"></p>
